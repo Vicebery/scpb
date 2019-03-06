29d86afcf8419b34c744e6ddc60d2ffb149a021a
github maekko <github 15071429552@163.com>
2019-03-06 20:04:42 +0800

    合并

--- src/main/java/com/scpb/test/practice.java
+++ src/main/java/com/scpb/test/practice.java
@@ -1,23 +1,6 @@
 package com.scpb.test;
 
-import java.util.Scanner;
-import java.util.Arrays;
-
 
 public class practice {
-    public static void main(String args[]){
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int k = in.nextInt();
-        int num[] = new int[n];
-        for(int i = 0;i<n;i++){
-            num[i]=in.nextInt();
 
-        }
-        Arrays.sort(num);
-        for(int i = 0;i<k;i++){
-            System.out.print(num[i]);
-            System.out.print(" ");
-        }
-    }
 }
